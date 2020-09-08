defe08d848d1b658a6d501abc4987179bad064ce
AVER343 <ankushvermagb@gmail.com>
2020-09-08 14:52:07 +0530

    commit

---
+++ client/build/precache-manifest.54d20505bc6ef2d104b5f0d2afd746bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4a6f61f4687734e7dc06fe8d8cce56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bca8e8c6deedb3ac4f08",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "19e38f25b3b0cf591489",
+    "url": "/static/css/main.e867e692.chunk.css"
+  },
+  {
+    "revision": "bca8e8c6deedb3ac4f08",
+    "url": "/static/js/2.bf6e91fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf6e91fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e38f25b3b0cf591489",
+    "url": "/static/js/main.6ace71c8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
