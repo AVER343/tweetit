c5164e80a1eeffec277ec804c0ee206f7f4729a4
AVER343 <ankushvermagb@gmail.com>
2020-09-08 15:07:37 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -20,6 +20,6 @@ app.use(express.static('client/build'))
 app.use("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
+app.listen(process.env.PORT||5000,()=>{
+    console.log(`Listening to port ${process.env.PORT||5000}`)
 })
