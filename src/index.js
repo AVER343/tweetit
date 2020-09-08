ecffc2d2d028d4d878a8ae896b971c1b2cb54c31
AVER343 <ankushvermagb@gmail.com>
2020-09-08 14:28:45 +0530

    'heroku'

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(Tweet)
 app.use(User)
-const PORT=5000
-app.listen(process.env.PORT||PORT,()=>{
+if(!process.env.PORT)
+{
+    process.env.PORT=5000
+}
+app.listen(process.env.PORT,()=>{
     console.log(`Listening to port ${PORT}`)
 })
